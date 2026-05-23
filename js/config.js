3fc4ac64b0766d9f51b1305d2b756f93976c2244
oliviagourmetalanya <oliviagourmetalanya@gmail.com>
2026-05-23 20:58:35 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /* ============================================
-   AYARLAR — Yönetim Paneli'nden 15.05.2026 18:30:31 tarihinde dışa aktarıldı
+   AYARLAR — Yönetim Paneli'nden 23.05.2026 20:58:05 tarihinde dışa aktarıldı
    ============================================ */
 
 window.CONFIG = {
